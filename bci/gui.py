69870696a27fe86845371427b73e20bf3c5bca42
Michael Glukhman <glukhman@mail.tau.ac.il>
2020-02-18 00:19:39 +0200

    Beautify GUI

--- bci/gui.py
+++ bci/gui.py
@@ -4,6 +4,7 @@ import click
 import requests
 import numpy as np
 from flask import Flask
+from flask import render_template
 from flask import send_from_directory
 import matplotlib.pyplot as plt, mpld3
 from mpl_toolkits.mplot3d import Axes3D
@@ -14,7 +15,7 @@ from pathlib import Path
 from .utils import VERSION, DATA_DIR
 
 
-website = Flask(__name__)
+website = Flask(__name__, static_folder='static')
 
 
 def _html(title, content):
@@ -28,65 +29,67 @@ def _html(title, content):
 
 @website.route('/')
 def index_():
+
     url = f"http://{website.config['api_host']}:{website.config['api_port']}/users"
-    users_list = json.loads(requests.get(url).json())
-    users_list = ['<li><a href="/users/{0}">{1}</a></li>'.format(
-            user['user_id'], user['username']
-        ) for user in users_list]
-    content = '<h1>Users</h1><ul>{}</ul>'.format(''.join(sorted(users_list)))
-    return _html('Welcome', content)
+    users = sorted(json.loads(requests.get(url).json()))
+    return render_template('index.html', users=users)
 
 
 @website.route('/users/<int:user_id>')
 def user(user_id):
-    url = f"http://{website.config['api_host']}:{website.config['api_port']}/users/{user_id}"
-    user_data = json.loads(requests.get(url).json())
-    username = user_data['username']
-    content = f"<h1>{username}</h1>{user_data['gender'].capitalize()}. Born on {user_data['birthday']}"
-    content += f"<h3>{username}'s snapshots:</h3>"
+    host = website.config['api_host']
+    port = website.config['api_port']
 
-    url = f"http://{website.config['api_host']}:{website.config['api_port']}/users/{user_id}/snapshots"
-    snapshots = json.loads(requests.get(url).json())
-
-    snapshots = ['<li><a href="/users/{0}/snapshots/{1}">{1}</a>&emsp;taken on {2}</li>'.format(
-            user_id, snapshot['id'], snapshot['datetime']
-        ) for snapshot in snapshots]
-    content += '<ul>{}</ul>'.format(''.join(sorted(snapshots)))
-    content += '<a href="/">Back</a>'
-    return _html(f'User {user_id}', content)
+    url = f"http://{host}:{port}/users/{user_id}"
+    user = json.loads(requests.get(url).json())
+    url = f"http://{host}:{port}/users/{user_id}/snapshots"
+    snapshots = sorted(json.loads(requests.get(url).json()))
+    for snapshot in snapshots:
+        url = f"http://{host}:{port}/users/{user_id}/snapshots/{snapshot['id']}"
+        available_results = json.loads(requests.get(url).json())['available results'].split(',')
+        available_results = [result.strip() for result in available_results]
+        if 'color_image' in available_results:
+            url = f"http://{host}:{port}/users/{user_id}/snapshots/{snapshot['id']}/color_image"
+            image_data = json.loads(requests.get(url).json())
+            snapshot['image_url'] = image_data['image_url']
+        else:
+            snapshot['image_url'] = '/static/missing.jpg'
+    return render_template('user.html', user=user, snapshots=snapshots)
 
 
 @website.route('/users/<int:user_id>/snapshots/<string:snapshot_id>')
 def snapshot(user_id, snapshot_id):
-    url = f"http://{website.config['api_host']}:{website.config['api_port']}/users/{user_id}"
-    user_data = json.loads(requests.get(url).json())
-    url = f"http://{website.config['api_host']}:{website.config['api_port']}/users/{user_id}/snapshots/{snapshot_id}"
-    snapshot_data = json.loads(requests.get(url).json())
-    print(snapshot_data)
-    content = f"<h1>Snapshot by {user_data['username']}</h1>"
-    content += f"Snapshot #{snapshot_data['id']} taken at {snapshot_data['datetime']}"
+    host = website.config['api_host']
+    port = website.config['api_port']
 
-    available_results = snapshot_data['available results'].split(',')
+    url = f"http://{host}:{port}/users/{user_id}"
+    user = json.loads(requests.get(url).json())
+    url = f"http://{host}:{port}/users/{user_id}/snapshots/{snapshot_id}"
+    snapshot = json.loads(requests.get(url).json())
+    snapshot['date'], snapshot['time'] = snapshot['datetime'].split('at')
+    available_results = json.loads(requests.get(url).json())['available results'].split(',')
     available_results = [result.strip() for result in available_results]
 
-    # show color image
+    # process color image
     if 'color_image' in available_results:
-        url = f"http://{website.config['api_host']}:{website.config['api_port']}/users/{user_id}/snapshots/{snapshot_id}/color_image"
+        url = f"http://{host}:{port}/users/{user_id}/snapshots/{snapshot['id']}/color_image"
         image_data = json.loads(requests.get(url).json())
-        print(image_data)
-        content += f"<p><img src='{image_data['image_url']}' alt='Color image' width='50%'>"
+        snapshot['color_image_url'] = image_data['image_url']
+    else:
+        snapshot['color_image_url'] = '/static/missing.jpg'
 
-    # show depth image
+    # process depth image
     if 'depth_image' in available_results:
-        url = f"http://{website.config['api_host']}:{website.config['api_port']}/users/{user_id}/snapshots/{snapshot_id}/depth_image"
+        url = f"http://{host}:{port}/users/{user_id}/snapshots/{snapshot_id}/depth_image"
         image_data = json.loads(requests.get(url).json())
-        print(image_data)
-        content += f"<p><img src='{image_data['image_url']}' alt='Depth image' width='30%'>"
+        snapshot['depth_image_url'] = image_data['image_url']
+    else:
+        snapshot['depth_image_url'] = '/static/missing.jpg'
 
-    # show pose
+    # process pose
     if 'pose' in available_results:
-        url = f"http://{website.config['api_host']}:{website.config['api_port']}/users/{user_id}/snapshots/{snapshot_id}/pose"
-        pose_data = json.loads(requests.get(url).json())
+        url = f"http://{host}:{port}/users/{user_id}/snapshots/{snapshot_id}/pose"
+        pose = json.loads(requests.get(url).json())
 
         # translation
         fig = plt.figure(figsize=(12,12), dpi=72)
@@ -95,9 +98,9 @@ def snapshot(user_id, snapshot_id):
         ax.set_ylim3d(-2,2)
         ax.set_zlim3d(-2,2)
 
-        x = pose_data['translation']['x']
-        y = pose_data['translation']['y']
-        z = pose_data['translation']['z']
+        x = pose['translation']['x']
+        y = pose['translation']['y']
+        z = pose['translation']['z']
         xline = np.linspace(x, 2, 100)
         yline = np.linspace(-2, y, 100)
         zline = np.linspace(-2, z, 100)
@@ -111,9 +114,9 @@ def snapshot(user_id, snapshot_id):
 
         datapath = DATA_DIR / 'translations'
         Path(datapath).mkdir(parents=True, exist_ok=True)
-        plt.savefig(f"{datapath}/{snapshot_data['id']}.png",
+        plt.savefig(f"{datapath}/{snapshot['id']}.png",
                     bbox_inches='tight', pad_inches=0, dpi=48)
-        content += f"<p><img src='/assets/translations/{snapshot_data['id']}.png' alt='Translation plot'>"
+        snapshot['translation_url'] = f"/assets/translations/{snapshot['id']}.png"
 
         # rotation
         fig = plt.figure(figsize=(12,12), dpi=72)
@@ -141,8 +144,8 @@ def snapshot(user_id, snapshot_id):
         ax.plot([0]*100, np.sin(c), np.cos(c), c='b', alpha=0.3,
                 ls='--')
 
-        rotation = [pose_data['rotation']['x'], pose_data['rotation']['y'],
-                    pose_data['rotation']['z'], pose_data['rotation']['w']]
+        rotation = [pose['rotation']['x'], pose['rotation']['y'],
+                    pose['rotation']['z'], pose['rotation']['w']]
         vector = pr.q_prod_vector(np.array(rotation),
                                   np.array([0,0,1]))
         ax.quiver(0,0,0,*vector, length=1, linewidth=5,color='k')
@@ -150,18 +153,23 @@ def snapshot(user_id, snapshot_id):
 
         datapath = DATA_DIR / 'rotations'
         Path(datapath).mkdir(parents=True, exist_ok=True)
-        plt.savefig(f"{datapath}/{snapshot_data['id']}.png",
+        plt.savefig(f"{datapath}/{snapshot['id']}.png",
                     bbox_inches='tight', pad_inches=0, dpi=48)
-        content += f"<p><img src='/assets/rotations/{snapshot_data['id']}.png' alt='Rotation plot'>"
+        snapshot['rotation_url'] = f"/assets/rotations/{snapshot['id']}.png"
+    else:
+        snapshot['translation_url'] = '/static/missing.jpg'
+        snapshot['rotation_url'] = '/static/missing.jpg'
 
-    # show feelings
+    # process feelings
     if 'feelings' in available_results:
-        url = f"http://{website.config['api_host']}:{website.config['api_port']}/users/{user_id}/snapshots/{snapshot_id}/feelings"
-        feelings_data = json.loads(requests.get(url).json())
-        content += f"<p>{feelings_data}"
+        url = f"http://{host}:{port}/users/{user_id}/snapshots/{snapshot_id}/feelings"
+        feelings = json.loads(requests.get(url).json())
+        # TODO ....
+    else:
+        pass
+        # TODO ....
 
-    content += f"<p><a href='/users/{user_id}'>Back to {user_data['username']}'s page</a>"
-    return _html(f'Snapshot', content)
+    return render_template('snapshot.html', user=user, snapshot=snapshot)
 
 
 @website.route('/assets/<string:type>/<string:asset_id>')
@@ -199,6 +207,7 @@ def _run_server(host, port, api_host, api_port):
         api_port = 8000
     website.config['api_host'] = api_host
     website.config['api_port'] = api_port
+    website.TEMPLATES_AUTO_RELOAD = True
     website.run(host, port)
 
 
